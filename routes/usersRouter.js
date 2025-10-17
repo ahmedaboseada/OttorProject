7d5d5b49d8e616b98998970dc784b788308120e7
ahmedaboseada <sci.ahmedemad49663@alexu.edu.eg>
2025-10-17 23:27:12 +0300

    fix deployment

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,7 +5,7 @@ import { validation } from '../middlewares/validation.js';
 import { createUserValidation, loginValidation } from '../validations/userValidation.js';
 import { verifyToken } from '../middlewares/authentication.js';
 
-userRouter.post('/register', validation(createUserValidation), userController.createUser);
+// userRouter.post('/register', validation(createUserValidation), userController.createUser);
 userRouter.post('/login', validation(loginValidation), userController.login);
 userRouter.post('/logout', verifyToken, userController.logout);
 userRouter.post('/refresh-token', userController.refreshToken);
